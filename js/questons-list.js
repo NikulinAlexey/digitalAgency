b80a69f78418641013e5952ae7683ed26f1a31b8
Aleksey Nikulin <donaldduckisnotdead@gmail.com>
2023-03-03 16:02:54 +0300

    feature: готов десктоп questions

---
+++ js/questons-list.js
@@ -0,0 +1,14 @@
+const questionsList = Array.from(document.querySelectorAll('.questions__list-title'));
+
+
+questionsList.forEach(function(question) {
+  question.addEventListener('click', function(evt) {
+    evt.target.closest('.questions__list-item')
+    .querySelector('.questions__list-paragraph')
+    .classList.toggle('questions__list-paragraph_type_active');
+
+    evt.target.closest('.questions__list-item')
+    .querySelector('.questions__icon')
+    .classList.toggle('questions__icon_type_active');
+  })
+})
