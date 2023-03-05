99eea65726a01367e3a4e041e242390358a037a5
Aleksey Nikulin <donaldduckisnotdead@gmail.com>
2023-03-05 23:07:56 +0300

    feature: десктоп блок process готов

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,11 @@
 const buttonConsultation = document.querySelector('.consultations__button');
+const buttonOrder = document.querySelector('.process__button');
+
 const popupConsultation = document.querySelector('.popup_type_consultation');
+const popupOrder = document.querySelector('.popup_type_order')
+
 const buttonClosePopupConsultation = document.querySelector('.popup__close-icon_type_consultation');
+const buttonClosePopupOrder = document.querySelector('.popup__close-icon_type_order');  
 
 function openPopup (popup) {
   popup.classList.add('popup_opened');
@@ -16,4 +21,12 @@ buttonConsultation.addEventListener('click', function() {
 
 buttonClosePopupConsultation.addEventListener('click', function() {
   closePopup(popupConsultation)
-})
+});
+
+buttonOrder.addEventListener('click', function() {
+  openPopup(popupOrder)
+});
+
+buttonClosePopupOrder.addEventListener('click', function() {
+  closePopup(popupOrder)
+});
