d90a17f480fd1941db5756f5973a34ffd490eb0f
Aleksey Nikulin <donaldduckisnotdead@gmail.com>
2023-03-24 21:01:24 +0300

    feature: все страница готовы, остались index.html корректировоки

--- js/index.js
+++ js/index.js
@@ -5,12 +5,16 @@ const menu = document.querySelector('.header__menu');
 $(function(){
   $('.clients__item-block').height($('.clients__item-block').width());
   $('.gratitude__item').height($('.gratitude__item').width()/1.3);
+  $('.team__item').height($('.team__image').width()*0.7);
 
   $(window).resize(function(){
     $('.clients__item-block').height($('.clients__item-block').width());
     $('.gratitude__item').height($('.gratitude__item').width()/1.3);
+    $('.team__item').height($('.team__image').width()*0.7);
   });
 });
+
+
 
 btnBurger.addEventListener('click', function () {
   btnBurger.classList.toggle('active');
--- js/qustionsList.js
+++ js/qustionsList.js
@@ -1,13 +1,23 @@
-const questionsList = Array.from(document.querySelectorAll('.questions__list-title'));
+const qustionsBlockSelector = '.questions__list';
+const qustionsItemSelector = '.questions__list-item';
+const paragraphActiveClass = 'questions__list-paragraph_type_active';
+const paragraph = document.querySelector('.qustions__list-paragraph')
+const questionTitleSelector = '.questions__list-title';
+const questionParagraphSelector = '.questions__list-paragraph';
 
-questionsList.forEach(function(question) {
-  question.addEventListener('click', function(evt) {
-    evt.target.closest('.questions__list-item')
-    .querySelector('.questions__list-paragraph')
-    .classList.toggle('questions__list-paragraph_type_active');
+let acc = document.querySelectorAll(".questions__list-item");
+let i;
 
-    evt.target.closest('.questions__list-item')
-    .querySelector('.questions__icon')
-    .classList.toggle('questions__icon_type_active');
-  })
-})
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.closest('.questions__list-paragraph').classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+
